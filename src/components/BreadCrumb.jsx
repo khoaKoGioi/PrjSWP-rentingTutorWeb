75766ee8332e1de852614f9d46c4a1ff0e5a7a23
snoopwee <snoopwee@users.noreply.github.com>
2024-06-07 15:49:41 +0700

    them trang classes list

---
+++ src/components/BreadCrumb.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Breadcrumbs } from "@material-tailwind/react";
+import { Link } from 'react-router-dom';
+
+const BreadcrumbsWithIcon = ({ pathnames }) => {
+  return (
+    <Breadcrumbs>
+      <Link to="/" className="opacity-60">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+        </svg>
+      </Link>
+      {pathnames.map((value, index) => {
+        const to = `/${pathnames.slice(0, index + 1).join('/')}`;
+
+        return index === pathnames.length - 1 ? (
+          <span key={to}>{value}</span>
+        ) : (
+          <Link key={to} to={to} className="opacity-60">
+            {value}
+          </Link>
+        );
+      })}
+    </Breadcrumbs>
+  );
+};
+
+export default BreadcrumbsWithIcon;
